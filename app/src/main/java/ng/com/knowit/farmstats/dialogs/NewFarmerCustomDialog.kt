b8721e0bd1d447a96032cd32f71a3cbf666fa599
John Paul <jpaul5246@gmail.com>
2020-05-01 13:12:20 +0100

    Finished the UI for creating new farmer

---
+++ app/src/main/java/ng/com/knowit/farmstats/dialogs/NewFarmerCustomDialog.kt
@@ -0,0 +1,92 @@
+package ng.com.knowit.farmstats.dialogs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.textfield.TextInputLayout
+import ng.com.knowit.farmstats.R
+import ng.com.knowit.farmstats.databinding.NewFarmerDialogLayoutBinding
+import ng.com.knowit.farmstats.utility.Utils
+
+class NewFarmerCustomDialog : DialogFragment() {
+
+    private lateinit var binding: NewFarmerDialogLayoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.AppTheme_FullScreenDialog)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.new_farmer_dialog_layout, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val dialog = dialog
+        if (dialog != null) {
+            val width = ViewGroup.LayoutParams.MATCH_PARENT
+            val height = ViewGroup.LayoutParams.MATCH_PARENT
+            dialog.window!!.setLayout(width, height)
+            dialog.window!!.setWindowAnimations(R.style.AppTheme_Slide)
+        }
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        val rootView = view.findViewById<View>(R.id.new_farmer_linear_layout)
+        val textInputLayouts: List<TextInputLayout> = Utils.findViewsWithType(
+            rootView, TextInputLayout::class.java
+        )
+        binding.newFarmerToolbar.inflateMenu(R.menu.new_farmer_menu)
+        binding.newFarmerToolbar.setTitleTextColor(resources.getColor(R.color.colorText))
+        binding.newFarmerToolbar.title = "Create new Farmer"
+        binding.newFarmerToolbar.setNavigationOnClickListener { dismiss() }
+        binding.newFarmerToolbar.setOnMenuItemClickListener {
+            var noErrors = true
+            for (textInputLayout in textInputLayouts) {
+                val editTextString =
+                    textInputLayout.editText!!.text.toString()
+                if (editTextString.isEmpty()) {
+                    textInputLayout.error = resources.getString(R.string.error_string)
+                    noErrors = false
+                } else {
+                    textInputLayout.error = null
+                }
+            }
+            if (noErrors) {
+                //save farmer
+
+                dismiss()
+            }
+            true
+        }
+    }
+
+
+    companion object {
+        private val TAG = NewFarmerCustomDialog::class.java.simpleName
+
+        fun display(fragmentManager: FragmentManager): NewFarmerCustomDialog {
+            val newFarmerCustomDialog = NewFarmerCustomDialog()
+            newFarmerCustomDialog.show(fragmentManager, TAG)
+            return newFarmerCustomDialog
+        }
+    }
+
+}
--- app/src/main/java/ng/com/knowit/farmstats/fragments/HomeFragment.kt
+++ app/src/main/java/ng/com/knowit/farmstats/fragments/HomeFragment.kt
@@ -5,21 +5,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import ng.com.knowit.farmstats.R
+import ng.com.knowit.farmstats.databinding.FragmentHomeBinding
+import ng.com.knowit.farmstats.dialogs.NewFarmerCustomDialog
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        binding.createFarmerCard.setOnClickListener {
+
+            NewFarmerCustomDialog.display(childFragmentManager)
+            //open dialog for farmer creation
+        }
+
+        binding.createFarmCard.setOnClickListener {
+            //open dialog for farm creation
+        }
+
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
