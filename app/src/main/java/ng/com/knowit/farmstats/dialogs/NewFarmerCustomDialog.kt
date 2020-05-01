8f0e608b79dd3f6326b58d36b44ecbf40679f714
John Paul <jpaul5246@gmail.com>
2020-05-01 18:32:12 +0100

    Implemented getting farmer image from the camera

--- app/src/main/java/ng/com/knowit/farmstats/dialogs/NewFarmerCustomDialog.kt
+++ app/src/main/java/ng/com/knowit/farmstats/dialogs/NewFarmerCustomDialog.kt
@@ -1,20 +1,33 @@
 package ng.com.knowit.farmstats.dialogs
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
+import com.bumptech.glide.Glide
 import com.google.android.material.textfield.TextInputLayout
 import ng.com.knowit.farmstats.R
 import ng.com.knowit.farmstats.databinding.NewFarmerDialogLayoutBinding
 import ng.com.knowit.farmstats.utility.Utils
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NewFarmerCustomDialog : DialogFragment() {
 
     private lateinit var binding: NewFarmerDialogLayoutBinding
+
+    private val TAG: String = NewFarmerCustomDialog::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,8 +89,69 @@ class NewFarmerCustomDialog : DialogFragment() {
             }
             true
         }
+
+
+        binding.farmerImage.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
     }
 
+
+    val REQUEST_TAKE_PHOTO = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(activity!!.packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri =
+                        FileProvider.getUriForFile(context!!, "ng.com.knowit.farmstats", it)
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                    Log.d(TAG, photoURI.toString())
+                    Glide.with(context!!).load(photoURI).into(binding.farmerImage)
+                }
+            }
+        }
+    }
+
+
+    lateinit var currentPhotoPath: String
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = activity!!.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            val imageBitmap = data!!.extras!!.get("data") as Bitmap
+            //imageView.setImageBitmap(imageBitmap)
+           //val path = data!!.extras!!.get(MediaStore.EXTRA_OUTPUT)
+
+            Log.d(TAG, "Got here" + imageBitmap.toString())
+        }
+    }*/
 
     companion object {
         private val TAG = NewFarmerCustomDialog::class.java.simpleName
