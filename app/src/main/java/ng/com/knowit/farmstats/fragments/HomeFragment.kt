cb4b8ad6d98c16e864670f99124dbec8e13c207e
John Paul <jpaul5246@gmail.com>
2020-05-04 17:54:13 +0100

    Set-up the chart to display statistics

--- app/src/main/java/ng/com/knowit/farmstats/fragments/HomeFragment.kt
+++ app/src/main/java/ng/com/knowit/farmstats/fragments/HomeFragment.kt
@@ -7,10 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
+import com.github.mikephil.charting.utils.MPPointF
 import ng.com.knowit.farmstats.R
 import ng.com.knowit.farmstats.databinding.FragmentHomeBinding
 import ng.com.knowit.farmstats.dialogs.NewFarmDialog
 import ng.com.knowit.farmstats.dialogs.NewFarmerCustomDialog
+
 
 /**
  * A simple [Fragment] subclass.
@@ -42,6 +48,40 @@ class HomeFragment : Fragment() {
             NewFarmDialog.display(childFragmentManager)
         }
 
+        loadPieChart(10, 0)
+
+
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun loadPieChart(numberOfFarms: Int, numberOfFarmers: Int) {
+
+        if (numberOfFarms == 0 && numberOfFarmers == 0) {
+            binding.noDataImageView.visibility = View.VISIBLE
+        } else {
+
+            binding.pieChart.visibility = View.VISIBLE
+
+            val list = ArrayList<PieEntry>()
+
+            list.add(PieEntry(numberOfFarmers.toFloat(), "Farmers"))
+            list.add(PieEntry(numberOfFarms.toFloat(), "Farms"))
+            val dataSet = PieDataSet(list, "Legend")
+
+            dataSet.setDrawIcons(false)
+            dataSet.sliceSpace = 3f
+            dataSet.iconsOffset = MPPointF(0F, 40F)
+            dataSet.selectionShift = 5f
+            dataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
+
+            val data = PieData(dataSet)
+            binding.pieChart.data = data
+
+            binding.pieChart.highlightValues(null)
+            binding.pieChart.invalidate()
+            binding.pieChart.description.text = "Farm-Farmers Chart"
+        }
+
+
     }
 }
